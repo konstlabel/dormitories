337a6ecaa8f4ff463a3fd5d67831ad88c97fa59c
konstlabel <kostiauvarov123@gmail.com>
2025-11-13 00:33:37 +0300

    init project

---
+++ src/main/java/com/konstl/dormitories/DormitoriesApplication.java
@@ -0,0 +1,13 @@
+package com.konstl.dormitories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormitoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konstl/dormitories/DormitoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konstl.dormitories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
